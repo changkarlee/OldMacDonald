dd99b15349a5c0817c7077c56db0e231d10f75c8
changkarlee <kac2000.c@gmail.com>
2017-10-24 10:53:22 -0700

    final code for old macdonald

--- Chick.java
+++ Chick.java
@@ -1,11 +1,13 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
      private String myType;     
-     private String mySound;      
-     public Chick(String type, String sound)     
+     private String mySound;
+     private String mySound2;
+     public Chick(String type, String sound, String sound2)     
      {         
          myType = type;         
          mySound = sound;     
+         mySound2 = sound2;
      }     
      public Chick()     
      {         
